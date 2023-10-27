bdbb8ba0bac710d7e3ba10587f24fc55793acc67
mothana404 <https://github.com/mothana404>
2023-10-27 15:58:04 +0300

    new

--- controllers/server.js
+++ controllers/server.js
@@ -2,54 +2,65 @@ const express = require("express");
 const app = express();
 const db = require(`../models/config`);
 const port = 8080;
+app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get('/home', async (req, res) => {
     try {
-      const result = await db.query('SELECT * FROM "user"');
+      let result = await db.query('SELECT users.user_name, blogs.blog_id, blogs.blog_title, blogs.image, blogs.place_name, blogs.place_location FROM users INNER JOIN blogs ON users.user_id = blogs.the_user;');
       res.json(result.rows);
     } 
     catch (err) {
       console.error(err);
-      res.status(500).send('Internal Server Error');
+      res.status(500).send('error in get the home');
     }
   });
-  
-  //delete
-  app.delete('/delete/:id', async (req, res) => {
-      const id = req.params.id; // Get the ID from the route parameter.
-    
-      try {
-        const query = 'DELETE FROM student WHERE id = $1';
-        const result = await db.query(query, [id]);
-    
-        if (result.rowCount === 1) {
-          res.json({ message: 'Record deleted successfully' });
-        } else {
-          res.status(404).json({ message: 'Record not found' });
-        }
-      } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-      }
-  });
-  
-  app.put('/employees/soft-delete/:id', async (req, res) => {
-      const employeeId = req.params.id;
-      const client = await pool.connect();
-    
-      try {
-        const query = 'UPDATE employees SET is_active = FALSE WHERE id = $1';
-        const values = [employeeId];
-    
-        await client.query(query, values);
-        res.status(200).json({ message: `Employee with ID ${employeeId} soft deleted.` });
-      } catch (error) {
-        console.error('Error performing soft delete for employee:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      } finally {
-        client.release();
-      }
-  });
+
+app.post('/addBlog', async (req, res) => {
+    const { blog_title, blog_description, the_user, image, place_name, place_location } = req.body;
+    try {
+        const query = `
+            INSERT INTO blogs (blog_title, blog_description, the_user, image, place_name, place_location)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING blog_id`;
+        const values = [blog_title, blog_description, the_user, image, place_name, place_location];
+
+        const result = await db.query(query, values);
+        const newBlogId = result.rows[0].blog_id;
+        res.status(201).json({ message: 'Blog added successfully', blog_id : newBlogId });
+    } catch (error) {
+        console.error('Error adding blog:', error);
+        res.status(500).json({ error: 'Failed to add the blog' });
+    }
+});
+
+
+app.get('/getblog/:id', async(req, res) => {
+    try{
+        const query = 'select * from blogs where blog_id = $1';
+        const blogId = req.params.id;
+        const result = await db.query(query, [blogId]); 
+        res.json(result.rows);
+    }catch (error){
+        console.error('Failed to get one blog: ', error);
+        res.status(500).json({ error: 'Failed to get one blog'});
+    }
+})
+//delete
+//   app.delete('/delete/:id', async (req, res) => {
+//       const id = req.params.id;
+//       try {
+//         const query = 'DELETE FROM blogs WHERE id = $1';
+//         const result = await db.query(query, [id]);
+//         if (result.rowCount === 1) {
+//           res.json({ message: 'Record deleted successfully' });
+//         } else {
+//           res.status(404).json({ message: 'Record not found' });
+//         }
+//       } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Internal Server Error');
+//       }
+//   });
 
 app.listen(port, () => {
     console.log(`server running in port ${port}`);
--- models/config.js
+++ models/config.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
-const dotenv = require("dotenv");
-dotenv.config();
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const pool = new Pool({
     user: 'postgres',
